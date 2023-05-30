d4045f577cfd0f514c3a07369c065b657515893a
gingerbradman <lordbreadly8@gmail.com>
2023-05-29 19:59:21 -0500

    Jam Update

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,16 +10,24 @@ public class MainMenu : MonoBehaviour
     public Image background;
     public Sprite backgroundSprite;
     public Sprite tutorialSprite;
+    public Sprite twotorialSprite;
 
     public GameObject mainmenuUI;
     public GameObject tutorialUI;
-
+    public GameObject twotorialUI;
 
     public void StartPressed()
     {
         background.sprite = tutorialSprite;
         mainmenuUI.SetActive(false);
         tutorialUI.SetActive(true);
+    }
+
+    public void ContinuePressed()
+    {
+        background.sprite = twotorialSprite;
+        tutorialUI.SetActive(false);
+        twotorialUI.SetActive(true);
     }
 
     public void LoadGivenScene(int x)
